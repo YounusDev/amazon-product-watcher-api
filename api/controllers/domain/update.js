0c1b2c50c3d26fcd8a188d3697735ef16fbe60dc
Abdullah <noman19bd@yahoo.com>
2020-03-16 14:45:47 +0600

    Update Project File Added

--- api/controllers/domain/update.js
+++ api/controllers/domain/update.js
@@ -1,30 +1,33 @@
 module.exports = async function (req, res) {
     let errors = {};
 
-    let firstName = req.param('first_name');
-    let lastName = req.param('last_name');
+    let projectName = req.param('project_name');
+    let domainUrl = req.param('domain_url');
+    let domainMeta = req.param('domain_meta');
+    let domainUseFor = req.param('domain_use_for');
 
-    if (!firstName) {
-        errors.firstName = 'first_name field is required';
+    if (!projectName) {
+        errors.firstName = 'project_name field is required';
     }
-    if (!lastName) {
-        errors.lastName = 'last_name field is required';
+    if (!domainUrl) {
+        errors.lastName = 'domain_url field is required';
     }
 
     if (Object.keys(errors).length) {
         return res.json({errors: errors}).status(422);
     }
 
-    await UserMeta.updateOne({userId: req.me.id})
+    await DomainMeta.updateOne({userId: req.me.id})
         .set({
-            firstName: firstName,
-            lastName: lastName
+            //firstName: firstName,
+            //lastName: lastName
         });
 
-    let userWithMeta = await User.withMeta({id: req.me.id});
-    delete userWithMeta.password;
+    let domainWithMeta = await Domain.withMeta({id: req.me.id});
+
+    //delete domainWithMeta.password;
 
     return res.json({
-        user: userWithMeta
+        domain: domainWithMeta
     }).status(200);
 };
