89252362fe2ee5ae9699d3c329fe8d3baece9aad
Younus <younusdev@gmail.com>
2020-04-04 17:07:12 +0600

    fix request validates response status

--- api/controllers/auth/register.js
+++ api/controllers/auth/register.js
@@ -1,21 +1,19 @@
 module.exports = async function (req, res) {
 
     //check already logged in
-    if(!!JWTHelpers.hasToken(req)) {
-        return res.forbidden();
-    }
+    if (!!JWTHelpers.hasToken(req)) return res.forbidden();
 
     if (!request.validate(req, res, {
-        'email': 'required|email',
+        'email'     : 'required|email',
         'first_name': 'required',
-        'last_name': 'required',
-        'password': 'required|confirmed'
+        'last_name' : 'required',
+        'password'  : 'required|confirmed'
     })) return;
 
-    let email = req.param('email');
+    let email     = req.param('email');
     let firstName = req.param('first_name');
-    let lastName = req.param('last_name');
-    let password = req.param('password');
+    let lastName  = req.param('last_name');
+    let password  = req.param('password');
 
     let checkUser = await User.findOne({
         email: email.toLowerCase()
@@ -23,43 +21,53 @@ module.exports = async function (req, res) {
 
     if (checkUser) {
         return res.status(422).json({
-            errors:
-                {'email': 'this email already used'}
+            errors: {'email': 'this email already used'}
         });
     }
 
     let createdUser = await User.create({
-        email: email,
-        password: await sails.helpers.passwords.hashPassword(password),
+        email    : email,
+        password : await sails.helpers.passwords.hashPassword(password),
         validated: true
     }).fetch();
 
     let userMeta = await UserMeta.create({
-        userId: createdUser.id,
+        userId   : createdUser.id,
         firstName: firstName,
-        lastName: lastName
+        lastName : lastName
     }).fetch();
 
     delete createdUser.password;
 
-    let appSecret = sails.config.custom.appSecret;
-    let jwtTime = sails.config.custom.jwtTime;
+    let appSecret      = sails.config.custom.appSecret;
+    let jwtTime        = sails.config.custom.jwtTime;
     let jwtRefreshTime = sails.config.custom.jwtRefreshTime;
 
-    let bearerToken = await sails.JWT.sign({
-        data: createdUser
-    }, appSecret, {expiresIn: jwtTime});
+    let bearerToken = await sails.JWT.sign(
+        {
+            data: createdUser
+        },
+        appSecret,
+        {expiresIn: jwtTime}
+    );
 
-    let refreshToken = await sails.JWT.sign({
-        data: {bearerToken: bearerToken, user: createdUser}
-    }, appSecret, {expiresIn: jwtRefreshTime / 1000});
+    let refreshToken = await sails.JWT.sign(
+        {
+            data: {
+                bearerToken: bearerToken,
+                user       : createdUser
+            }
+        },
+        appSecret,
+        {expiresIn: jwtRefreshTime / 1000}
+    );
 
-    res.cookie('refreshToken', refreshToken, { maxAge: jwtRefreshTime, httpOnly: true, signed:true });
+    res.cookie('refreshToken', refreshToken, {maxAge: jwtRefreshTime, httpOnly: true, signed: true});
 
     createdUser.meta = userMeta;
 
     return res.status(200).json({
-        user: createdUser,
+        user       : createdUser,
         bearerToken: bearerToken,
     });
 };
