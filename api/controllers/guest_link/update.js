5a437575d00cfdea6789c66f8673324b6c20107d
Abdullah <noman19bd@yahoo.com>
2020-04-15 17:55:30 +0600

    Guest Post Links Updated

--- api/controllers/guest_link/update.js
+++ api/controllers/guest_link/update.js
@@ -2,6 +2,9 @@ module.exports = async function (req, res) {
 
     let userProjectId = req.param('id');
     let status = req.param('status');
+    let myUrl = req.param('my_url');
+    let remoteUrl = req.param('remote_url');
+    let linkInformation = req.param('link_information');
 
     let userProject = await UserDomain.findOne({
         id    : userProjectId,
@@ -21,10 +24,38 @@ module.exports = async function (req, res) {
             }
         });
 
+    if (myUrl) {
+        await CheckLinkInGuestDomain.updateOne({userId: req.me.id, usersDomainsId: userProject.id})
+            .set({
+                url : {
+                    myUrl
+                }
+            });
+    }
 
-    let updatedGuestPost = await UserDomain.withDomain({userId: req.me.id, id: userProject.id});
+    if (remoteUrl) {
+        await CheckLinkInGuestDomain.updateOne({userId: req.me.id, usersDomainsId: userProject.id})
+            .set({
+                remoteUrl : {
+                    remoteUrl
+                }
+            });
+    }
+
+    if (linkInformation) {
+        await CheckLinkInGuestDomain.updateOne({userId: req.me.id, usersDomainsId: userProject.id})
+            .set({
+                linkInfo    : {
+                    linkInformation
+                }
+            });
+    }
+
+    let guestLinkInfo = await CheckLinkInGuestDomain.find({
+        usersDomainsId: userProject.id,
+    });
 
     return res.status(200).json({
-        guestPosts: updatedGuestPost
+        guestPosts: guestLinkInfo
     });
 }
