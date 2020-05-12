0d02579ae1a2521b6b3c3ce5e1bc181badad2181
Younus <younusdev@gmail.com>
2020-05-12 20:11:10 +0600

    fix broken links lookup

--- api/controllers/broken_link/index.js
+++ api/controllers/broken_link/index.js
@@ -1,58 +1,47 @@
 module.exports = async function (req, res) {
-
+    
     let projectId = req.param('project_id');
-
+    
     let userDomain = await UserDomain.findOne({
-        id: projectId,
+        id    : projectId,
         userId: req.me.id
     });
-
+    
     if (!userDomain) {
         return res.status(404).json({message: 'invalid project id'});
     }
-
+    
     // use db helpers get func only for normal find. nor findOne. only find support given
     let pagesResult = await dbHelpers.get(
         Page.pageAggregated,
         {
-            where: {domainId: userDomain.domainId},
-            // lookup: {
-            //     from: 'pages_meta',
-            //     let: {id: '$_id'},
-            //     pipeline: [
-            //         {
-            //             $match: {
-            //                 $expr: {
-            //                     $and: {
-            //                         $eq: [{$toString: '$$id'}, '$page_id']
-            //                     }
-            //                 }
-            //             }
-            //         }
-            //     ],
-            //     as: 'meta'
-            // }
+            where : {domainId: userDomain.domainId},
+            lookup: {
+                from    : 'pages_meta',
+                let     : {id: '$_id'},
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: {
+                                $eq: [
+                                    {$toString: '$$id'},
+                                    '$page_id'
+                                ]
+                            }
+                        }
+                    }
+                ],
+                as      : 'meta'
+            },
+            // cant use this cz page_meta can be empty
+            // unwind: '$meta'
+            unwind: {
+                path: '$meta',
+                preserveNullAndEmptyArrays: true
+            }
         },
         req
     );
-
-    let pages = pagesResult.data;
-
-    if (!pages) {
-        return res.status(200).json('broken links not found');
-    }
-
-    await Promise.all(Object.keys(pages).map(async (objKey) => {
-        page = pages[objKey];
-
-        let pageMeta = await PageMeta.find({
-            pageId: page['id']
-        });
-
-        page.meta = pageMeta;
-
-        return page;
-    }));
-
+    
     return res.status(200).json(pagesResult);
 };
--- api/services/dbHelpers.js
+++ api/services/dbHelpers.js
@@ -2,25 +2,26 @@ module.exports.get = async function (model, options = {}, req = null, limit = 1)
     let queryOptions = [];
     let skip         = 0;
     let current_page = 1;
-
+    
     if (req) {
         if (_.has(req.query, 'page')) {
             skip         = (parseInt(req.query.page) - 1) * limit;
             current_page = parseInt(req.query.page);
         }
     }
-
+    
     if (!_.has(options, 'limit')) queryOptions.limit = limit;
-
+    
     let originalMatchQuery = !_.has(options, 'where') ? _.cloneDeep(options) : _.cloneDeep(options.where);
-    // let lookupQuery = !_.has(options, 'lookup') ? null : _.cloneDeep(options.lookup);
-
+    let lookupQuery        = !_.has(options, 'lookup') ? null : _.cloneDeep(options.lookup);
+    let unwindQuery        = !_.has(options, 'lookup') ? null : _.cloneDeep(options.unwind);
+    
     let finalMatchQuery = {};
-
+    
     Object.keys(originalMatchQuery).map(key => {
         finalMatchQuery[_.snakeCase(key)] = originalMatchQuery[key];
     });
-
+    
     queryOptions = [
         {
             $match: finalMatchQuery,
@@ -31,9 +32,6 @@ module.exports.get = async function (model, options = {}, req = null, limit = 1)
                     {
                         $match: finalMatchQuery,
                     },
-                    // {
-                    //     $lookup: lookupQuery
-                    // },
                     {
                         $skip: skip
                     },
@@ -61,25 +59,36 @@ module.exports.get = async function (model, options = {}, req = null, limit = 1)
             }
         }
     ];
+    
+    if (lookupQuery) {
+        // push lookup query at 2nd position
+        queryOptions[1].$facet.data.splice(1, 0, {$lookup: lookupQuery});
+    }
+    if (unwindQuery) {
+        // push lookup query at 3rd position
+        // its now only for getting single single item. dont use unwind if you has more than 1 elm for now
+        queryOptions[1].$facet.data.splice(2, 0, {$unwind: unwindQuery});
+    }
+    
     let result = await model(queryOptions);
-
+    
     result.data.map(obj => {
         if (_.hasIn(obj, '_id')) {
             obj.id = String(obj._id);
-
+            
             delete obj._id;
         }
     });
-
-
+    
+    
     if (result && _.hasIn(result, 'pagination_meta')) {
         let paginationMeta = result.pagination_meta;
         let last_page      = Math.ceil(parseInt(paginationMeta.total) / limit);
-
+        
         paginationMeta.last_page = last_page || 0;
     }
-
+    
     if (result && !_.hasIn(result.pagination_meta, 'total')) result.pagination_meta.total = 0;
-
+    
     return result;
 };
