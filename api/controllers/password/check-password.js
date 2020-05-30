29e51ccf3e02c61367fc53b4d2ec212512bd8623
Abdullah al mamun <abdullah.ssc13@gmail.com>
2020-05-30 17:27:29 +0600

    change password field name changed

--- api/controllers/password/check-password.js
+++ api/controllers/password/check-password.js
@@ -1,7 +1,7 @@
 module.exports = async function (req, res) {
 
     if (!request.validate(req, res, {
-        'password': 'required'
+        'current_password': 'required'
     })) return;
 
     let userRecord = await User.findOne({
@@ -9,7 +9,7 @@ module.exports = async function (req, res) {
     });
 
     try {
-        await sails.helpers.passwords.checkPassword(req.param('password'), userRecord.password);
+        await sails.helpers.passwords.checkPassword(req.param('current_password'), userRecord.password);
     } catch (e) {
         return res.status(404).json({message: 'password does not match'});
     }
