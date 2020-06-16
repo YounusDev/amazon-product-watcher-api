04bb202d3e3903e3db76c03a4a2367578227df28
Younus <younusdev@gmail.com>
2020-06-16 15:36:18 +0600

    remove affiliate id support

--- api/controllers/amazon_product/update.js
+++ api/controllers/amazon_product/update.js
@@ -1,28 +1,26 @@
 module.exports = async function (req, res) {
     let userProjectId = req.param('id');
-    let affiliateIds  = req.param('affiliate_ids');
     let status        = req.param('status');
-    
+
     let userProject = await UserDomain.findOne({
         id    : userProjectId,
         userId: req.me.id
     });
-    
+
     if (!userProject) {
         return res.status(404).json({message: 'invalid project id'});
     }
-    
+
     // it will try to preserve domain use for nested objects
     // cz updated one cant update only a single key of a nested object
     let prevDomainUseFor = _.cloneDeep(userProject.domainUseFor);
     if (!prevDomainUseFor.hasOwnProperty('amazonProductsCheckService')) {
         prevDomainUseFor['amazon_products_check_service'] = {};
     }
-    
-    let modifiedDomainUseFor                                            = prevDomainUseFor;
-    modifiedDomainUseFor.amazon_products_check_service['affiliate_ids'] = affiliateIds;
-    modifiedDomainUseFor.amazon_products_check_service['status']        = status;
-    
+
+    let modifiedDomainUseFor                                     = prevDomainUseFor;
+    modifiedDomainUseFor.amazon_products_check_service['status'] = status;
+
     let updatedProjectService = await UserDomain.updateOne({
         userId: req.me.id,
         id    : userProject.id
@@ -30,7 +28,7 @@ module.exports = async function (req, res) {
         .set({
             domainUseFor: modifiedDomainUseFor
         });
-    
+
     return res.status(200).json({
         project: commonHelpers.objectKeysToSnakeCase(updatedProjectService)
     });
